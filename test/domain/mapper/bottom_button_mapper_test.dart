e877c6fa66502f446aa02ce3e401025062cab291
Thiago Cardoso <thiago.cardoso@nubank.com.br>
2021-06-10 09:28:21 -0300

    Aplicando tema do NuDS ao MaterialApp

---
+++ test/domain/mapper/bottom_button_mapper_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nos_codamos/data/model/bottom_button.dart';
+import 'package:nos_codamos/domain/mapper/bottom_button_mapper.dart';
+
+void main() {
+  testWidgets('Should map the BdcBottomButton to a BottomBar with success', (WidgetTester tester) async {
+    final text = 'teste';
+    BdcBottomButton bdcBottomButton = BdcBottomButton(
+        text: text,
+        style: 'no_style',
+        action:
+            BdcBottomButtonAction(method: 'post', uri: '/submit', steps: []));
+
+    var bottomBar = BottomButtonWidgetMapper.map(bdcBottomButton, () {});
+
+    tester.pumpWidget(bottomBar);
+
+    expect(find.text(text), findsOneWidget);
+  });
+}
